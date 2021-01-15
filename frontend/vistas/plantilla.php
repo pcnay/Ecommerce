e2428f56ddf155949723575bdceafd86e6c7b13e
Ramon <pcnay2003@gmail.com>
2021-01-15 02:09:06 +0000

    Se agregan carpetas y de define la plantilla.php basica incial para el proyecto

--- frontend/vistas/plantilla.php
+++ frontend/vistas/plantilla.php
@@ -2,7 +2,16 @@
 	<html lang="es">
 		<head>
 			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0, minimun-scale=1.0,maximum-scale=1.0, user-scalable=no">>
+			<meta name="title" content="Tienda Virtual">
+			<meta name="descripcion" content="Curso Ecommerce">
+			<meta name="keyword" content = "Este curso es para aprender a realizar un Ecommerce Basico">
 			<title>Tienda Virtual</title>
+			<link rel="stylesheet" href="css/plugins/bootstrap.min.css">
+			<link rel="stylesheet" href="css/plugins/font-awesome.min.css">
+			<script src="js/plugins/jquery.min.js"></script>
+			<script src="js/plugins/bootstrap.min.js"></script>
+
 		</head>
 		<body>
 			<h1>Titulo</h1>
