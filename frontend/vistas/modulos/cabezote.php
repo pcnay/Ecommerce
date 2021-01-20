57f8c9f9eb82cbef8c9542c42590f25c8a2ada6a
Ramon <pcnay2003@gmail.com>
2021-01-20 02:04:48 +0000

    Se continua con la maquetacion del Cabezote, se esta eliminado carpetas y agregando otras

--- frontend/vistas/modulos/cabezote.php
+++ frontend/vistas/modulos/cabezote.php
@@ -1,13 +1,96 @@
 
 <!-- Se comienza agragar el archivo HTML del Ecommerce --> 
 <!-- TOP --> 
-<div class="container-fluid" id="top">
+<div class="container-fluid barraSuperior" id="top">
 	<div class = "container">
-		<div>
-		</div>
+		<div class="row">
+			<!--  Sección para Redes Sociales 
+				SE aplica Responsive en las pantallas, varios tamaños
+			-->
+			<div class = "col-lg-9 col-md-9 col-sm-8 col-xs-12 social">
+				<ul>
+		
+					<li>
+						<a href="http://facebook.com/" target="_blank">
+							<!-- Se utiliza fontaweson para los iconos de la redes sociales --> 
+							<i class="fa fa-facebook redSocial facebookBlanco" aria-hidden="true"></i>
+						</a>
+					</li>
 
+					<li>
+						<a href="http://youtube.com/" target="_blank">
+							<i class="fa fa-youtube redSocial youtubeBlanco" aria-hidden="true"></i>
+						</a>
+					</li>
+
+					<li>
+						<a href="http://twitter.com/" target="_blank">
+							<i class="fa fa-twitter redSocial twitterBlanco" aria-hidden="true"></i>
+						</a>
+					</li>
+
+					<li>
+						<a href="http://google.com/" target="_blank">
+							<i class="fa fa-google-plus redSocial googleBlanco" aria-hidden="true"></i>
+						</a>
+					</li>
+
+					<li>
+						<a href="http://instagram.com/" target="_blank">
+							<i class="fa fa-instagram redSocial instagramBlanco" aria-hidden="true"></i>
+						</a>
+					</li>
+
+				</ul>
+
+			</div>
+
+			<!-- Seccion Para Registro de Usuario -->
+			<div class = "col-lg-3 col-md-3 col-sm-4 col-xs-12 registro">
+				<ul>
+				
+					<li><a href="#modalIngreso" data-toggle="modal">Ingresar</a></li>
+					<li>|</li>
+					<li><a href="#modalRegistro" data-toggle="modal">Crear una cuenta</a></li>
+
+				</ul>
+
+			</div>
+
+		</div> <!-- <div class="row"> -->
 
 	</div> <!-- <div class = "container"> -->
 
 </div> <!-- class="container-fluid" id="top"> -->
 	
+<!-- Header -->
+<header class= "container-fluid">
+	<div class = "container">
+		<div class="row" id="cabezote">
+
+			<!-- LOGOTIPO-->
+			<div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="logotipo">				
+				<a href="#">						
+					<img src="http://162.0.238.251/ecommerce/backend/vistas/img/plantilla/logo.png" class="img-responsive">
+				</a>				
+			</div> <!-- <div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="logotipo"> -->
+
+			<!-- Categorias y Buscador -->
+			<div class="col-lg-6 col-md-6 col-sm-8 col-xs-12">
+				
+				<!-- Boton de Categorias -->
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 backColor" id="btnCategorias">					
+					<p>CATEGORÍAS
+						<span class="pull-right">
+							<i class="fa fa-bars" aria-hidden="true"></i>
+						</span>					
+					</p>
+				</div> <!-- <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 backColor" id="btnCategorias"> -->
+
+			</div> <!-- <div class="col-lg-6 col-md-6 col-sm-8 col-xs-12"> -->
+
+		</div> <!-- <div class="row"> -->
+
+	</div> <!-- <div class = "container"> -->
+
+</header>
--- frontend/vistas/plantilla.php
+++ frontend/vistas/plantilla.php
@@ -7,10 +7,10 @@
 			<meta name="descripcion" content="Curso Ecommerce">
 			<meta name="keyword" content = "Este curso es para aprender a realizar un Ecommerce Basico">
 			<title>Tienda Virtual</title>
-			<link rel="stylesheet" href="css/plugins/bootstrap.min.css">
-			<link rel="stylesheet" href="css/plugins/font-awesome.min.css">
-			<script src="js/plugins/jquery.min.js"></script>
-			<script src="js/plugins/bootstrap.min.js"></script>
+			<link rel="stylesheet" href="vistas/css/plugins/bootstrap.min.css">
+			<link rel="stylesheet" href="vistas/css/plugins/font-awesome.min.css">
+			<script src="vistas/js/plugins/jquery.min.js"></script>
+			<script src="vistas/js/plugins/bootstrap.min.js"></script>
 
 		</head>
 		<body>
